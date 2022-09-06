455f4637bea4753a71b1e1b90760f48bc50156b9
Alireza Talakoobi <alirezatalakoobi@gmail.com>
2022-09-06 12:23:20 +0200

    finish mqtt

--- rpi/MyMQTT.py
+++ rpi/MyMQTT.py
@@ -17,3 +17,39 @@ class MyMQTT:
         # register the callback
         self._paho_mqtt.on_connect = self.myOnConnect
         self._paho_mqtt.on_message = self.myOnMessageReceived
+
+    def myOnConnect (self, paho_mqtt, userdata, flags, rc):
+        print ("Connected to %s with result code: %d" % (self.broker, rc))
+
+    def myOnMessageReceived (self, paho_mqtt , userdata, msg):
+        # A new message is received
+        self.notifier.notify (msg.topic, msg.payload)
+
+    def myPublish (self, topic, msg):
+        # if needed, you can do some computation or error-check before publishing
+        print ("publishing '%s' with topic '%s'" % (msg, topic))
+        # publish a message with a certain topic
+        self._paho_mqtt.publish(topic, msg, 2)
+
+    def mySubscribe (self, topic):
+        # if needed, you can do some computation or error-check before subscribing
+        print ("subscribing to %s" % (topic))
+        # subscribe for a topic
+        self._paho_mqtt.subscribe(topic, 2)
+
+        # just to remember that it works also as a subscriber
+        self._isSubscriber = True
+        self._topic = topic
+
+    def start(self):
+        #manage connection to broker
+        self._paho_mqtt.connect(self.broker , self.port)
+        self._paho_mqtt.loop_start()
+
+    def stop (self):
+        if (self._isSubscriber):
+            # remember to unsuscribe if it is working also as subscriber 
+            self._paho_mqtt.unsubscribe(self._topic)
+
+        self._paho_mqtt.loop_stop()
+        self._paho_mqtt.disconnect()
