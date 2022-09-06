0d4e437054c5cb167fd0dd3899e1fe234e4b5886
Alireza Talakoobi <alirezatalakoobi@gmail.com>
2022-09-06 11:29:19 +0200

    initialization of my mqtt

---
+++ rpi/MyMQTT.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as PahoMQTT
+
+
+class MyMQTT:
+    def __init__(self, clientID, broker, port, notifier):
+        self.broker = broker
+        self.port = port
+        self.notifier = notifier
+        self.clientID = clientID
+
+        self._topic = ""
+        self._isSubscriber = False
+
+        # create an instance of paho.mqtt.client
+        self._paho_mqtt = PahoMQTT.Client(clientID, False) 
+
+        # register the callback
+        self._paho_mqtt.on_connect = self.myOnConnect
+        self._paho_mqtt.on_message = self.myOnMessageReceived
